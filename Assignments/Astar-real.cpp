489cd25c0f9c69566629075b7be8450856af5122
RitikaAvadhanula <ritika.avadhanula@sirabtech.com>
2021-04-01 11:23:53 -0700

    assignments added

--- Assignments/Astar-real.cpp
+++ Assignments/Astar-real.cpp
@@ -1,30 +1,85 @@
-// shortest - with robot orientation
 #include <iostream>
-#include <string.h>
+#include <math.h>
 #include <vector>
 #include <algorithm>
+#include <fstream>
 
 using namespace std;
 
 // Map class
 class Map {
 public:
-    const static int mapWidth = 6;
-    const static int mapHeight = 5;
-    vector<vector<int> > grid = {
-        { 0, 1, 0, 0, 0, 0 },
-        { 0, 1, 0, 0, 0, 0 },
-        { 0, 1, 0, 0, 0, 0 },
-        { 0, 1, 0, 0, 0, 0 },
-        { 0, 0, 0, 1, 1, 0 }
-    };
+    const static int mapHeight = 300;
+    const static int mapWidth = 150;
+    vector<vector<double> > map = GetMap();
+    vector<vector<int> > grid = MaptoGrid();
+    vector<vector<int> > heuristic = GenerateHeuristic();
+
+private:
+    // Read the file and get the map
+    vector<vector<double> > GetMap()
+    {
+        vector<vector<double> > mymap(mapHeight, vector<double>(mapWidth));
+        ifstream myReadFile;
+        myReadFile.open("map.txt");
+
+        while (!myReadFile.eof()) {
+            for (int i = 0; i < mapHeight; i++) {
+                for (int j = 0; j < mapWidth; j++) {
+                    myReadFile >> mymap[i][j];
+                }
+            }
+        }
+        return mymap;
+    }
+
+    //Convert the map to 1's and 0's
+    vector<vector<int> > MaptoGrid()
+    {
+        vector<vector<int> > grid(mapHeight, vector<int>(mapWidth));
+        for (int x = 0; x < mapHeight; x++) {
+            for (int y = 0; y < mapWidth; y++) {
+                if (map[x][y] == 0) //unkown state
+                    grid[x][y] = 1;
+
+                else if (map[x][y] > 0) //Occupied state
+                    grid[x][y] = 1;
+
+                else //Free state
+                    grid[x][y] = 0;
+            }
+        }
+
+        return grid;
+    }
+
+    // Generate a Manhattan Heuristic Vector
+    vector<vector<int> > GenerateHeuristic()
+    {
+        vector<vector<int> > heuristic(mapHeight, vector<int>(mapWidth));
+        int goal[2] = { 60, 50 };
+        for (int i = 0; i < heuristic.size(); i++) {
+            for (int j = 0; j < heuristic[0].size(); j++) {
+                int xd = goal[0] - i;
+                int yd = goal[1] - j;
+                // Manhattan Distance
+                   int d = abs(xd) + abs(yd);
+                // Euclidian Distance
+                // double d = sqrt(xd * xd + yd * yd);
+                // Chebyshev distance
+                // int d = max(abs(xd), abs(yd));
+                heuristic[i][j] = d;
+            }
+        }
+        return heuristic;
+    }
 };
 
 // Planner class
 class Planner : Map {
 public:
-    int start[2] = { 0, 0 };
-    int goal[2] = { mapHeight - 1, mapWidth - 1 };
+    int start[2] = { 230, 145 };
+    int goal[2] = { 60, 50 };
     int cost = 1;
 
     string movements_arrows[4] = { "^", "<", "v", ">" };
@@ -35,9 +90,11 @@ public:
         { 1, 0 },
         { 0, 1 }
     };
+
+    vector<vector<int> > path;
 };
 
-// Template function to print 2D vectors of any type
+// Printing vectors of any type
 template <typename T>
 void print2DVector(T Vec)
 {
@@ -49,8 +106,7 @@ void print2DVector(T Vec)
     }
 }
 
-// Search function will generate the expansions
-void search(Map map, Planner planner)
+Planner search(Map map, Planner planner)
 {
     // Create a closed 2 array filled with 0s and first element 1
     vector<vector<int> > closed(map.mapHeight, vector<int>(map.mapWidth));
@@ -62,16 +118,17 @@ void search(Map map, Planner planner)
     // Create action array filled with -1
     vector<vector<int> > action(map.mapHeight, vector<int>(map.mapWidth, -1));
 
-    // Defined the triplet values
+    // Defined the quadruplet values
     int x = planner.start[0];
     int y = planner.start[1];
     int g = 0;
+    int f = g + map.heuristic[x][y];
 
     // Store the expansions
     vector<vector<int> > open;
-    open.push_back({ g, x, y });
+    open.push_back({ f, g, x, y });
 
-    // Flags and counters
+    // Flags and Counts
     bool found = false;
     bool resign = false;
     int count = 0;
@@ -88,7 +145,7 @@ void search(Map map, Planner planner)
         }
         // Keep expanding
         else {
-            // Remove triplets from the open list
+            // Remove quadruplets from the open list
             sort(open.begin(), open.end());
             reverse(open.begin(), open.end());
             vector<int> next;
@@ -96,13 +153,14 @@ void search(Map map, Planner planner)
             next = open.back();
             open.pop_back();
 
-            x = next[1];
-            y = next[2];
-            g = next[0];
+            x = next[2];
+            y = next[3];
+            g = next[1];
 
             // Fill the expand vectors with count
             expand[x][y] = count;
             count += 1;
+            
 
             // Check if we reached the goal:
             if (x == planner.goal[0] && y == planner.goal[1]) {
@@ -118,7 +176,8 @@ void search(Map map, Planner planner)
                     if (x2 >= 0 && x2 < map.grid.size() && y2 >= 0 && y2 < map.grid[0].size()) {
                         if (closed[x2][y2] == 0 and map.grid[x2][y2] == 0) {
                             int g2 = g + planner.cost;
-                            open.push_back({ g2, x2, y2 });
+                            f = g2 + map.heuristic[x2][y2];
+                            open.push_back({ f, g2, x2, y2 });
                             closed[x2][y2] = 1;
                             action[x2][y2] = i;
                         }
@@ -127,6 +186,7 @@ void search(Map map, Planner planner)
             }
         }
     }
+
     // Print the expansion List
     //print2DVector(expand);
 
@@ -141,23 +201,27 @@ void search(Map map, Planner planner)
     while (x != planner.start[0] or y != planner.start[1]) {
         x2 = x - planner.movements[action[x][y]][0];
         y2 = y - planner.movements[action[x][y]][1];
+        // Store the  Path in a vector
+        planner.path.push_back({ x2, y2 });
         policy[x2][y2] = planner.movements_arrows[action[x][y]];
         x = x2;
         y = y2;
     }
 
-    // Print the path with arrows
+    // Print the robot path
+    //cout << endl;
     print2DVector(policy);
+
+    return planner;
 }
 
 int main()
 {
-    // Instantiate map and planner objects
+    // Instantiate a planner and map objects
     Map map;
     Planner planner;
-
-    // Search for the expansions
-    search(map, planner);
+    // Generate the shortest Path using the Astar algorithm
+    planner = search(map, planner);
 
     return 0;
 }
--- Assignments/search_expansion.cpp
+++ Assignments/search_expansion.cpp
@@ -8,6 +8,18 @@
 As you expand towards a new cell, check if you have reached the goal; and once you reach it, print its triplet value.
 Actively check if you have reached a roadblock. If you do, stop expanding and print a message indicating that you have failed to reach the goal.
 Expand the cells with the lowest g value and store your expansions in an open vector. If two cells have equal g values, then you can to pick one of them to expand further.
+Each cell in the expansion vector will store the order at which it was expanded. 
+Some of the cells were never expanded, and should show a value of -1.
+Expansion #: 11
+Open List: [10 1 3 ], [10 0 2 ], [9 3 4 ], [9 2 3 ]
+Cell Picked: [9 2 3]
+
+The final step is to print the shortest path that the robot has to take in order to travel 
+from start to goal. You will need to record each action that the robot should take
+(ex: turning left <) and store all the actions in a policy 2D vector.
+
+
+
 */
 
 #include <iostream>
@@ -61,10 +73,83 @@ void print2DVector(T Vec)
 }
 
 /*#### TODO: Code the search function which will generate the expansion list ####*/
+/* #### TODO: Modify the search function to generate and print the expansion 2D vector #### */
+
 // You are only required to print the final triplet values
 void search(Map map, Planner planner)
 {
+    // Create a closed 2 array filled with 0s and first element 1
+    vector<vector<int> > closed(map.mapHeight, vector<int>(map.mapWidth));
+    closed[planner.start[0]][planner.start[1]] = 1;
+
+    // Defined the triplet values
+    int x = planner.start[0];
+    int y = planner.start[1];
+    int g = 0;
+
+    // Store the expansions
+    vector<vector<int> > open;
+    open.push_back({ g, x, y });
+
+    // Flags
+    bool found = false;
+    bool resign = false;
+
+    int x2;
+    int y2;
+    int count = 0;
+    // Create expand array filled with -1
+    vector<vector<int> > expand(map.mapHeight, vector<int>(map.mapWidth, -1));
     
+    // While I am still searching for the goal and the problem is solvable
+    while (!found && !resign) {
+        // Resign if no values in the open list and you can't expand anymore
+        if (open.size() == 0) {
+            resign = true;
+            cout << "Failed to reach a goal" << endl;
+        }
+        // Keep expanding
+        else {
+            // Remove triplets from the open list
+            sort(open.begin(), open.end());
+            reverse(open.begin(), open.end());
+            vector<int> next;
+            // Stored the poped value into next
+            next = open.back();
+            open.pop_back();
+
+            x = next[1];
+            y = next[2];
+            g = next[0];
+
+            // Fill the expand vectors with count
+            expand[x][y] = count;
+            count += 1;
+            
+            // Check if we reached the goal:
+            if (x == planner.goal[0] && y == planner.goal[1]) {
+                found = true;
+                cout << "[" << g << ", " << x << ", " << y << "]" << endl;
+            }
+
+            //else expand new elements
+            else {
+                for (int i = 0; i < planner.movements.size(); i++) {
+                    x2 = x + planner.movements[i][0];
+                    y2 = y + planner.movements[i][1];
+                    if (x2 >= 0 && x2 < map.grid.size() && y2 >= 0 && y2 < map.grid[0].size()) {
+                        if (closed[x2][y2] == 0 and map.grid[x2][y2] == 0) {
+                            int g2 = g + planner.cost;
+                            open.push_back({ g2, x2, y2 });
+                            closed[x2][y2] = 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    // Print the expansion List
+    print2DVector(expand);
 }
 
 int main()
